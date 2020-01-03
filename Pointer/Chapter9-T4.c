2176224e2be1b7b35bd49599cdada7364428b15f
Tenderest <2087886585@qq.com>
2020-01-03 12:55:05 +0800

    :tada: C Primer Plus Chapter9 Programming Exercises-T4

---
+++ Pointer/Chapter9-T4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "pointer.h"
+/* compile with pointer.c */
+int main(void)
+{
+    printf("%.3lf and %.3lf = %.3lf\n", 3.513, 4.341, Tiaohe(3.513, 4.341));
+    return 0;
+}
